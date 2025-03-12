23ac11805be8b2ce1248aa6e4c88baae199c061e
GPiorek <GrzegorzPiorekkk@outlook.com>
2025-03-12 13:18:53 +0100

    WPRG adding third exercise

--- WPRG/Zjazd_1.php
+++ WPRG/Zjazd_1.php
@@ -2,20 +2,50 @@
 
 // Zad 2 - Liczby Pierwsze
 echo("Prime Numbers \n");
-
-$n = 100;
-for ($i = 2; $i <= $n; $i++) {
-    $k = $i ** (1 / 2);
-    $m = floor($k);
-    $o = true;
-    for ($l = 2; $l <= $m; $l++) {
-        if ($i % $l == 0) {
-            $o = false;
-            break;
+primeNumbers();
+function primeNumbers()
+{
+    $n = 100;
+    for ($i = 2; $i <= $n; $i++) {
+        $k = $i ** (1 / 2);
+        $m = floor($k);
+        $o = true;
+        for ($l = 2; $l <= $m; $l++) {
+            if ($i % $l == 0) {
+                $o = false;
+                break;
+            }
+        }
+        if ($o) {
+            echo($i . " ");
         }
     }
-    if ($o) {
-        echo($i . " ");
+    echo("\n\n");
+}
+
+
+//Zad 3 Ciąg Fibonacciego
+echo("Fibonacci" . "\n");
+fib(15);
+function fib($n)
+{
+
+    $j = 1;
+    $k = 1;
+    $tab = array($j);
+
+    for ($i = 1; $i < $n; $i++) {
+        $l = $k + $j;
+        $tab[] = $k;
+        $j = $k;
+        $k = $l;
+
+    }
+
+    foreach ($tab as $key => $value) {
+        if ($value % 2 == 0) {
+            echo("1. " . $value . "\n");
+        }
     }
 }
 
