c1751d393c6ba4e877fc609433fbcced9e135541
GPiorek <GrzegorzPiorekkk@outlook.com>
2025-03-12 11:12:22 +0100

    Adding WPRG exercise n2

--- WPRG/Zjazd_1.php
+++ WPRG/Zjazd_1.php
@@ -1,3 +1,22 @@
 <?php
 
+// Zad 2 - Liczby Pierwsze
+echo("Prime Numbers \n");
+
+$n = 100;
+for ($i = 2; $i <= $n; $i++) {
+    $k = $i ** (1 / 2);
+    $m = floor($k);
+    $o = true;
+    for ($l = 2; $l <= $m; $l++) {
+        if ($i % $l == 0) {
+            $o = false;
+            break;
+        }
+    }
+    if ($o) {
+        echo($i . " ");
+    }
+}
+
 
