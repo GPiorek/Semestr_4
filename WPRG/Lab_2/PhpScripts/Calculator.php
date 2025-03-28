4a45a8029897d19036fefa9cf28057a77551cfbe
GPiorek <GrzegorzPiorekkk@outlook.com>
2025-03-28 20:59:53 +0100

    Little fixes

--- WPRG/Lab_2/PhpScripts/Calculator.php
+++ WPRG/Lab_2/PhpScripts/Calculator.php
@@ -19,9 +19,43 @@ switch ($operationSign) {
         break;
 }
 if ($firstNumber == 0 && $operationSign == "/" || $secondNumber == 0 && $operationSign == "/") {
-    echo "U cant divide by zero";
+    $message = "U cant divide by zero";
 } else {
-    echo "Result of operation<br>";
-    echo $firstNumber . " " . $operationSign . " " . $secondNumber . " = " . $result;
-
+    $message = "Result of operation is<br><br>" . $firstNumber . " " . $operationSign . " " . $secondNumber . " = " . $result;
 }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+</head>
+<body>
+<div id="resultOfOperation">
+    <div id="messageBlock">
+        <?php echo $message ?>
+    </div>
+</div>
+</body>
+</html>
+
+<style>
+    #resultOfOperation {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        font-size: 30px;
+        background-color: #4cdec8;
+        height: 100vh;
+    }
+
+    #messageBlock {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        height: 30%;
+        width: 30%;
+        background-color: rgba(0, 0, 0, 0.5);
+        border-radius: 30px;
+    }
+</style>
